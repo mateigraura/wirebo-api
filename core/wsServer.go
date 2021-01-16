d5ce1cde6a898f6ee53ec5029ecbe04157e2022e
mateigraura <grauramatei@gmail.com>
2021-01-16 15:07:51 +0200

    replace fiber with gin [socket conn hijacked]

--- core/wsServer.go
+++ core/wsServer.go
@@ -1,10 +1,27 @@
 package core
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+const pubSubChannel = "pub-sub-chan"
 
 type WsServer struct {
+	clients    map[uuid.UUID]bool
+	rooms      map[uuid.UUID]bool
 	register   chan *WsClient
 	unregister chan *WsClient
+}
+
+func NewWsServer() *WsServer {
+	return &WsServer{
+		clients:    make(map[uuid.UUID]bool),
+		rooms:      make(map[uuid.UUID]bool),
+		register:   make(chan *WsClient),
+		unregister: make(chan *WsClient),
+	}
 }
 
 func (w *WsServer) Run() {
@@ -20,11 +37,15 @@ func (w *WsServer) Run() {
 }
 
 func (w *WsServer) registerClient(client *WsClient) {
-
+	w.clients[client.Id] = true
+	fmt.Printf("Welcome %s\n", client.Name)
 }
 
 func (w *WsServer) unregisterClient(client *WsClient) {
-
+	if _, ok := w.clients[client.Id]; ok {
+		delete(w.clients, client.Id)
+	}
+	fmt.Printf("See you %s\n", client.Name)
 }
 
 func (w *WsServer) findRoomById(id uuid.UUID) *Room {
